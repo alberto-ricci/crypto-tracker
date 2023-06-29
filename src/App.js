a8b8a71b198c08470ccfc864cbb5baa84be7f69d
Alberto Ricci <ricci.alberto.89@gmail.com>
2023-06-29 22:05:53 +0300

    second iteration

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Select from "react-select";
 import useFetch from "./useFetch";
 import styles from "./app.module.css";
+import { AiOutlineDelete } from "react-icons/ai";
+import { FiEdit2 } from "react-icons/fi"; // Import edit icon
 
 function App() {
   const url =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false";
   const { data: cryptos, isLoading } = useFetch(url);
-  const [wallet, setWallet] = useState([]);
+  const [wallet, setWallet] = useState(
+    JSON.parse(localStorage.getItem("wallet")) || []
+  );
   const [selectedCrypto, setSelectedCrypto] = useState();
   const [holdings, setHoldings] = useState("");
 
+  // Function to handle selected option in select component
   const handleSelect = (selectedOption) => {
     setSelectedCrypto(selectedOption);
   };
 
+  // Function to delete card
+  const deleteCard = (id) => {
+    setWallet(wallet.filter((crypto) => crypto.id !== id));
+  };
+
+  // Store wallet data in localStorage every time it changes
+  useEffect(() => {
+    localStorage.setItem("wallet", JSON.stringify(wallet));
+  }, [wallet]);
+
+  // Function to add crypto to wallet
   const addToWallet = () => {
     if (
       selectedCrypto &&
@@ -22,6 +38,15 @@ function App() {
     ) {
       setWallet([...wallet, { ...selectedCrypto, holdings }]);
     }
+  };
+
+  // Function to edit holdings of a crypto in wallet
+  const editHoldings = (id, newHoldings) => {
+    setWallet(
+      wallet.map((crypto) =>
+        crypto.id === id ? { ...crypto, holdings: newHoldings } : crypto
+      )
+    );
   };
 
   const today = new Date().toDateString();
@@ -72,21 +97,35 @@ function App() {
                 className={styles.logo}
               />
               <h2 className={styles.cryptoName}>{crypto.name}</h2>
-              <p
-                className={
-                  crypto.price_change_percentage_24h > 0
-                    ? styles.positive
-                    : styles.negative
-                }
-              >
-                {crypto.price_change_percentage_24h.toFixed(2)}% (24h)
-              </p>
+              <div className={styles.cryptoInfo}>
+                <p
+                  className={
+                    crypto.price_change_percentage_24h > 0
+                      ? styles.positive
+                      : styles.negative
+                  }
+                >
+                  {crypto.price_change_percentage_24h.toFixed(2)}% (24h)
+                </p>
+                <div className={styles.iconContainer}>
+                  <AiOutlineDelete
+                    className={styles.deleteIcon}
+                    onClick={() => deleteCard(crypto.id)}
+                  />
+                  <FiEdit2
+                    className={styles.editIcon}
+                    onClick={() =>
+                      editHoldings(crypto.id, prompt("Enter new holdings:"))
+                    }
+                  />
+                </div>
+              </div>
             </div>
             <div className={styles.holdingsContainer}>
-              <h3>Your Holdings</h3>
+              <h3>Holdings</h3>
               <p>
-                {crypto.holdings} * €{crypto.current_price} = €
-                {(crypto.holdings * crypto.current_price).toFixed(2)}
+                {crypto.holdings} {crypto.symbol.toUpperCase()} ={" "}
+                {(crypto.holdings * crypto.current_price).toFixed(2)} €
               </p>
             </div>
           </div>
